060c21740d6cf6e0c17e81df334edd15572304c5
git config --global user.name your_username <111802162+SThieshen@users.noreply.github.com>
2022-10-11 19:02:07 -0500

    adding deliverable tables

--- schema.sql
+++ schema.sql
@@ -108,3 +108,60 @@ FROM retirement_info as ri
 LEFT JOIN dept_info as di
 ON ri.emp_no = di.emp_no
 WHERE di.dept_name = ('Sales');
+
+-- Joining retirement_info and dept_info tables using left join and filter(s)
+SELECT ri.emp_no,
+    ri.first_name,
+	ri.last_name,
+	di.dept_name
+INTO duo_dept
+FROM retirement_info as ri
+LEFT JOIN dept_info as di
+ON ri.emp_no = di.emp_no
+WHERE di.dept_name IN ('Sales', 'Development');
+
+-- Follow the instructions below to complete Deliverable 1.
+SELECT e.emp_no,
+       e.first_name,
+       e.last_name,
+       t.title,
+       t.from_date,
+       t.to_date
+INTO retirement_titles
+FROM employees as e
+INNER JOIN titles as t
+ON (e.emp_no = t.emp_no)
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+ORDER BY e.emp_no;
+
+-- Use Distinct with Orderby to remove duplicate rows
+SELECT DISTINCT ON (emp_no) emp_no, 
+	first_name, 
+    last_name, 
+    title
+INTO unique_titles
+FROM retirement_titles
+ORDER BY emp_no ASC, to_date DESC;
+
+-- retrieve the number of employees by their most recent job title who are about to retire.
+SELECT COUNT(title), title 
+INTO retiring_titles
+FROM unique_titles
+GROUP BY title
+ORDER BY COUNT(title) DESC;
+
+
+
+
+
+# CHALLENGE STARTER CODE
+-- Use Dictinct with Orderby to remove duplicate rows
+SELECT DISTINCT ON (______) _____,
+______,
+______,
+______
+
+INTO nameyourtable
+FROM _______
+WHERE _______
+ORDER BY _____, _____ DESC;
